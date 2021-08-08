aec50aae4c9005001601e755528d127133e61ba0
Júlia Brito <julialbritto@gmail.com>
2021-08-08 14:52:08 -0300

    commit

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -14,20 +14,31 @@ interface CardAnimationProps extends ViewProps {
 
 export function CardAnimation({ children, ...rest }: CardAnimationProps) {
   const { width: displayWidth } = useWindowDimensions();
+
   const cardOpacity = useSharedValue(0);
   const cardOffset = useSharedValue(0.25 * displayWidth);
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      // TODO - setup animated style
+      opacity: cardOpacity.value,
+      transform: [
+        {
+          translateX: cardOffset.value  
+        }
+      ]
     }
   })
 
   useEffect(() => {
-    /**
-     * TODO - setup cardOpacity.value and cardOffset.value with
-     * withTiming()
-     */
+    cardOpacity.value = withTiming(
+      1,
+      { duration: 1000 }
+    );
+
+    cardOffset.value = withTiming(
+      0,
+      { duration: 1000 }
+    );
   }, []);
 
   return (
